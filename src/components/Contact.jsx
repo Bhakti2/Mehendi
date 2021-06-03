9f9a1ee0ce2f7f56f42b43c7856fafa97054a6c4
Bhakti Dilkhush <bhaktidilkhush.2000@gmail.com>
2021-06-03 15:44:52 +0530

    refactor contcat again

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,77 +1,136 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      email: '',
-      message: ''
-    }
+      name: "",
+      email: "",
+      message: "",
+    };
   }
 
-  handleSubmit(e){
+  handleSubmit(e) {
     e.preventDefault();
     axios({
       method: "POST",
-      url:"http://localhost:3002/send",
-      data:  this.state
-    }).then((response)=>{
-      if (response.data.status === 'success') {
-        alert("Message Sent.");
-        this.resetForm()
-      } else if (response.data.status === 'fail') {
-        alert("Message failed to send.")
+      url: "http://localhost:3002/send",
+      data: this.state,
+    }).then((response) => {
+      //   <div>
+      //   { window.location.hash === '#success' &&
+      //     <div id="success">
+      //       <p>Your message has been sent!</p>
+      //     </div>
+      //   }
+      //   { window.location.hash === '#error' &&
+      //     <div id="error">
+      //       <p>An error occured while submitting the form.</p>
+      //     </div>
+      //   }
+      // </div>
+
+      if (response.data.status === "success") {
+        {
+          window.location.hash === "#success" && (
+            <div id="success">
+              <p>Your message has been sent!</p>
+            </div>
+          );
+        }
+        this.resetForm();
+      } else if (response.data.status === "fail") {
+        {
+          window.location.hash === "#error" && (
+            <div id="error">
+              <p>An error occured while submitting the form.</p>
+            </div>
+          );
+        }
       }
-    })
+    });
   }
 
-  resetForm(){
-    this.setState({name: "", email: "", message: ""})
+  resetForm() {
+    this.setState({ name: "", email: "", message: "" });
   }
 
   render() {
-    return(
+    return (
       <div>
+        <h1 className="CardHeading">Send us Meassage</h1>
 
-     <h1  className="CardHeading">
-       Send us Meassage
-     </h1>
-     
-      <div className="App fromOutterCon">
-        <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST"className="Myform">
-          <div className="form-group">
+        <div className="App fromOutterCon">
+          <form
+            id="contact-form"
+            onSubmit={this.handleSubmit.bind(this)}
+            method="POST"
+            className="Myform"
+          >
+            <div className="form-group">
               <label htmlFor="name">Name</label>
-              <input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
-          </div>
-          <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={this.state.name}
+                onChange={this.onNameChange.bind(this)}
+              />
+            </div>
+            <div className="form-group">
               <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
-          </div>
-          <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                value={this.state.email}
+                onChange={this.onEmailChange.bind(this)}
+              />
+            </div>
+            <div className="form-group">
               <label htmlFor="message">Message</label>
-              <textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
-          </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </form>
-      </div>
+              <textarea
+                className="form-control"
+                rows="5"
+                id="message"
+                value={this.state.message}
+                onChange={this.onMessageChange.bind(this)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
 
+            <div>
+              {window.location.hash === "#success" && (
+                <div id="success">
+                  <p>Your message has been sent!</p>
+                </div>
+              )}
+              {window.location.hash === "#error" && (
+                <div id="error">
+                  <p>An error occured while submitting the form.</p>
+                </div>
+              )}
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
 
   onNameChange(event) {
-	  this.setState({name: event.target.value})
+    this.setState({ name: event.target.value });
   }
 
   onEmailChange(event) {
-	  this.setState({email: event.target.value})
+    this.setState({ email: event.target.value });
   }
 
   onMessageChange(event) {
-	  this.setState({message: event.target.value})
+    this.setState({ message: event.target.value });
   }
 }
 
